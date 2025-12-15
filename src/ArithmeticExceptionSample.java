7bec1aa2fdeaf91e118404c23f60cbf559ded432
ke13n <shayu8984@gmail.com>
2025-12-15 12:47:56 +0900

    Javaプログラミング入門学習終了

---
+++ src/ArithmeticExceptionSample.java
@@ -0,0 +1,14 @@
+
+public class ArithmeticExceptionSample {
+
+    public static void main(String[] args) {
+        // int型10をint型0で除算
+        int a = 10 / 0;
+        
+        //この下の出力は実行されない
+        System.out.println(a);
+        
+
+    }
+
+}
---
+++ src/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("TechAcademy");
+        System.out.println("Java");
+        System.out.println("学習を始めましょう!");
+        //これは表示されない
+        /*
+         かこっているところは表示されない
+         dd
+         */
+        System.out.println("hello warld");
+        
+    }
+}
